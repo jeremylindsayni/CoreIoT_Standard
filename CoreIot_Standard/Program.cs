d60bbabafba58164863cfa126b23a82bd1038252
jeremylindsayni <jeremy.lindsay@outlook.com>
2017-04-02 21:58:50 +0100

    Added basic console project for an IoT "Hello World" application which references a .NET Standard library.

---
+++ CoreIot_Standard/Program.cs
@@ -0,0 +1,15 @@
+﻿using StringLib;
+using System;
+
+namespace RaspberryPiCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printOut = MyStrings.HelloWorld;
+
+            Console.WriteLine(printOut);
+        }
+    }
+}
---
+++ StringLib/MyStrings.cs
@@ -0,0 +1,7 @@
+﻿namespace StringLib
+{
+    public class MyStrings
+    {
+        public static string HelloWorld = "Hello internet - this is a .NET Standard project";
+    }
+}
